35c139b8b7152cbf9f3fc28c2a460ef39eee97e9
Jaisal E. K. <mail@jaisal.in>
2026-05-23 21:38:31 +0000

    Feat: Implement chronicle table for schema versioning and lifecycle tracking

--- src/ServiceDatabase.pas
+++ src/ServiceDatabase.pas
@@ -40,8 +40,10 @@ type
   TServiceDatabase = class
   private
     FDB: TStaticSQLite;
-    procedure EnsureSchema;
+    function GetChronicleValue(const AKey, ADefault: String): String;
     procedure CleanupOldBackups;
+    procedure EnsureSchema;
+    procedure SetChronicleValue(const AKey, AValue: String);
   public
     constructor Create;
     destructor Destroy; override;
@@ -51,7 +53,7 @@ type
 implementation
 
 uses
-  MonoLexID;
+  AppIdentity, MonoLexID;
 
 constructor TServiceDatabase.Create;
 var
@@ -66,10 +68,39 @@ begin
   CleanupOldBackups;
 end;
 
-procedure TServiceDatabase.EnsureSchema;
+function TServiceDatabase.GetChronicleValue(const AKey, ADefault: String): String;
 var
   Res: TDBResult;
 begin
+  Result := ADefault;
+  Res := FDB.Query('SELECT value FROM chronicle WHERE key = ' + QuotedStr(AKey));
+  if Length(Res) > 0 then
+    Result := Res[0][0];
+end;
+
+procedure TServiceDatabase.SetChronicleValue(const AKey, AValue: String);
+begin
+  FDB.Exec('INSERT OR REPLACE INTO chronicle (key, value) VALUES (' + QuotedStr(AKey) + ', ' + QuotedStr(AValue) + ');');
+end;
+
+procedure TServiceDatabase.EnsureSchema;
+var
+  Res: TDBResult;
+  IsV100208Upgrade: Boolean;
+begin
+  FDB.Exec('CREATE TABLE IF NOT EXISTS chronicle (key TEXT PRIMARY KEY, value TEXT, updated_at DATETIME NOT NULL DEFAULT (STRFTIME(''%Y-%m-%d %H:%M:%f'', ''now'')));');
+  FDB.Exec('CREATE TRIGGER IF NOT EXISTS trg_chronicle_updated_at AFTER UPDATE OF value ON chronicle BEGIN UPDATE chronicle SET updated_at = STRFTIME(''%Y-%m-%d %H:%M:%f'', ''now'') WHERE key = OLD.key; END;');
+  if GetChronicleValue('SchemaVersion', '') = '' then
+  begin
+    Res := FDB.Query('SELECT name FROM sqlite_master WHERE type=''table'' AND name=''collections''');
+    IsV100208Upgrade := Length(Res) > 0;
+    SetChronicleValue('InstanceID', NewMonoLexID);
+    SetChronicleValue('SchemaVersion', '1');
+    if IsV100208Upgrade then
+      SetChronicleValue('CreatedByVersion', '1.0.0.208')
+    else
+      SetChronicleValue('CreatedByVersion', APP_VERSION);
+  end;
   FDB.Exec('CREATE TABLE IF NOT EXISTS collections (id TEXT PRIMARY KEY, name TEXT UNIQUE COLLATE NOCASE, created_at DATETIME DEFAULT CURRENT_TIMESTAMP);');
   FDB.Exec('CREATE TABLE IF NOT EXISTS definitions (id TEXT PRIMARY KEY, collection_id TEXT, name TEXT, trigger_word TEXT UNIQUE COLLATE NOCASE, definition_text TEXT, last_triggered DATETIME, trigger_count INTEGER DEFAULT 0, FOREIGN KEY(collection_id) REFERENCES collections(id) ON DELETE CASCADE);');
   FDB.Exec('CREATE INDEX IF NOT EXISTS idx_def_monolexid ON definitions(id);');
@@ -80,6 +111,7 @@ begin
   Res := FDB.Query('SELECT COUNT(*) FROM collections');
   if (Length(Res) > 0) and (StrToIntDef(Res[0][0], 0) = 0) then
     FDB.Exec('INSERT INTO collections (id, name) VALUES (' + QuotedStr(NewMonoLexID) + ', ''Default'');');
+  SetChronicleValue('LastOpenedByVersion', APP_VERSION);
 end;
 
 procedure TServiceDatabase.CleanupOldBackups;
